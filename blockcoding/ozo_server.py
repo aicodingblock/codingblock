9da19249c6b1e7a5aa24ce17f9bde36aa93da529
aicodingblock <69446789+aicodingblock@users.noreply.github.com>
2020-08-28 19:43:17 +0900

    ozobot python3 lib update

--- blockcoding/ozo_server.py
+++ blockcoding/ozo_server.py
@@ -1,7 +1,8 @@
-# from ozolib import ozolib
+import sys
 import ozolib
 import time
 import socket
+from threading import Thread
 
 DEBUG = False
 def log(s):
@@ -18,6 +19,122 @@ except OSError as e:
     print(e)
     exit(1)
 log("server started")
+
+status="start"
+stopflag = 0
+
+
+def t1(id,f,p1):
+    global status
+    status = 'waiting'
+    ret = f(p1)
+    status  = ret    
+
+def t2(id,f,p1,p2):
+    global status
+    status = 'waiting'
+    ret = f(p1,p2)
+    status  = ret    
+
+def t3(id,f,p1,p2,p3):
+    global status
+    status = 'waiting'
+    ret = f(p1,p2,p3)
+    status  = ret    
+
+def t4(id,f,p1,p2,p3,p4):
+    global status
+    status = 'waiting'
+    ret = f(p1,p2,p3,p4)
+    status  = ret
+    log("T4 ended\n\r")
+
+def tz(id,degree,count,dir=0):
+    global status,stopflag
+    status = 'waiting'
+    stopflag = 0
+    time = 300
+    if dir == 0:
+        speed = 1000
+    else:
+        speed = -1000
+    for i in range(0,count):
+        if stopflag==1:
+            return True
+        bot1.turn(degree)
+        if stopflag==1:       
+            return True        
+        bot1.move(speed,speed,time)
+        if stopflag==1:        
+            return True        
+        bot1.turn(-degree)
+        if stopflag==1:
+            return True        
+        bot1.move(speed,speed,time) 
+        if stopflag==1:
+            return True        
+    status = True
+
+def td(id):
+    global status,stopflag
+    status = 'waiting'
+    stopflag = 0
+
+    bot1.flashLED(0xff,0xff,0,3)        
+    bot1.play('allright1')
+    if stopflag==1:         
+        return True              
+    bot1.turn(1080,480)        
+    if stopflag==1:        
+        return True      
+    bot1.flashrainbow(3)
+    if stopflag==1:        
+        return True   
+    for i in range(0,2):  
+        if stopflag==1:                 
+            return True                       
+        bot1.zigzag(-60, 1, False)
+
+    bot1.flashLED(0,0xff,0,3)            
+    if stopflag==1:        
+        return True
+    bot1.turn(360)     
+    if stopflag==1:          
+        return True
+    bot1.play('allright2') 
+    if stopflag==1:           
+        return True    
+    bot1.rainbow()
+    if stopflag==1:         
+        return True  
+     
+    duration=200 
+    speed=1500
+    for j in range(0,2):               
+        for i in range(0,2):
+            if stopflag==1:          
+                return True 
+            bot1.move(speed,speed,duration)
+            if stopflag==1:         
+                return True            
+            bot1.move(-speed,-speed,duration)
+            if stopflag==1:        
+                return True
+            bot1.turn(30)
+    if stopflag==1:         
+        return True 
+    bot1.play('laugh5')  
+    if stopflag==1:       
+        return True  
+    bot1.turn(1440, 720)    
+    if stopflag==1:         
+        return True                                             
+    bot1.play('sad1')  
+    if stopflag==1:          
+        return True     
+    bot1.offLED() 
+    status = True
+
 
 while True:
     server_socket.listen(1) 
@@ -80,19 +197,11 @@ while True:
             else:
                 ret =  "Incorrect Parameters"  
 
-        elif com[0]=='maru_ostop':
-            if 'bot1' not in locals():
-                ret = False
-            elif param_cnt==1:
-                ret = bot1.bstop()
-            else:
-                ret =  "Incorrect Parameters" 
-
         elif com[0]=='maru_oreadcolor':
             if 'bot1' not in locals():
                 ret = False
-            elif param_cnt==1:
-                ret = bot1.breadcolor()
+            elif param_cnt==2:
+                ret = bot1.breadcolor(com[1])
             else:
                 ret =  "Incorrect Parameters" 
 
@@ -115,19 +224,34 @@ while True:
             else:
                 ret =  "Incorrect Parameters" 
 
+        elif com[0]=='maru_oallstop':
+            if 'bot1' not in locals():
+                ret = False
+            elif param_cnt==1:             
+                if status=='waiting':
+                    stopflag=1
+                    status = True
+                ret = bot1.ballstop()
+            else:
+                ret =  "Incorrect Parameters"                 
+
         elif com[0]=='maru_omove1':
             if 'bot1' not in locals():
                 ret = "Not Connected"  
-            elif param_cnt==5:                           
-                ret = bot1.bmove1(int(com[1]),int(com[2]),int(com[3]),int(com[4]))                              
+            elif param_cnt==5:                              
+                th_omove1=Thread(target=t4, args=(1, bot1.bmove1,int(com[1]),int(com[2]),int(com[3]),int(com[4])))
+                th_omove1.start()
+                ret=status                                          
             else:
                 ret =  "Incorrect Parameters"    
 
         elif com[0]=='maru_omove2':
             if 'bot1' not in locals():
                 ret = "Not Connected"  
-            elif param_cnt==4:                           
-                ret = bot1.bmove2(int(com[1]),int(com[2]),int(com[3]))                              
+            elif param_cnt==4:                             
+                th_omove1=Thread(target=t3, args=(2, bot1.bmove2,int(com[1]),int(com[2]),int(com[3])))
+                th_omove1.start()
+                ret=status
             else:
                 ret =  "Incorrect Parameters"                      
 
@@ -135,9 +259,13 @@ while True:
             if 'bot1' not in locals():
                 ret = "Not Connected"  
             elif param_cnt==2:                           
-                ret = bot1.bmove3(int(com[1]))  
+                th_omove3=Thread(target=t1, args=(3, bot1.bmove3,int(com[1])))
+                th_omove3.start() 
+                ret=status                               
             elif param_cnt==3:                           
-                ret = bot1.bmove3(int(com[1]),int(com[2]))                              
+                th_omove3=Thread(target=t2, args=(4, bot1.bmove3,int(com[1]),int(com[2])))
+                th_omove3.start()                                            
+                ret=status                
             else:
                 ret =  "Incorrect Parameters"   
 
@@ -145,7 +273,9 @@ while True:
             if 'bot1' not in locals():
                 ret = "Not Connected"  
             elif param_cnt==3:                           
-                ret = bot1.brotate1(int(com[1]),int(com[2]))                              
+                th_orotate1=Thread(target=t2, args=(5, bot1.brotate1,int(com[1]),int(com[2])))
+                th_orotate1.start()                                            
+                ret=status                                            
             else:
                 ret =  "Incorrect Parameters"  
 
@@ -153,7 +283,9 @@ while True:
             if 'bot1' not in locals():
                 ret = "Not Connected"  
             elif param_cnt==3:                           
-                ret = bot1.brotate2(int(com[1]),int(com[2]))                              
+                th_orotate2=Thread(target=t2, args=(6, bot1.brotate2,int(com[1]),int(com[2])))
+                th_orotate2.start()                                            
+                ret=status                                              
             else:
                 ret =  "Incorrect Parameters"  
 
@@ -168,7 +300,7 @@ while True:
         elif com[0]=='maru_otopled1':
             if 'bot1' not in locals():
                 ret = "Not Connected"  
-            elif param_cnt==4:                           
+            elif param_cnt==4:   
                 ret = bot1.btopled1(int(com[1]),int(com[2]),int(com[3]))                              
             else:
                 ret =  "Incorrect Parameters"        
@@ -176,7 +308,7 @@ while True:
         elif com[0]=='maru_otopled2':
             if 'bot1' not in locals():
                 ret = "Not Connected"  
-            elif param_cnt==1:                           
+            elif param_cnt==1:                          
                 ret = bot1.btopled2()                              
             else:
                 ret =  "Incorrect Parameters"   
@@ -257,15 +389,17 @@ while True:
             if 'bot1' not in locals():
                 ret = "Not Connected"  
             elif param_cnt==3:                           
-                ret = bot1.bgentone1(int(com[1]),str(com[2]))                              
+                ret = bot1.bgentone2(int(com[1]),str(com[2]))                              
             else:
                 ret =  "Incorrect Parameters"    
 
         elif com[0]=='maru_ogentone2':
             if 'bot1' not in locals():
                 ret = "Not Connected"  
-            elif param_cnt==4:                           
-                ret = bot1.bgentone2(int(com[1]),str(com[2]),float(com[3]))                              
+            elif param_cnt==4:                            
+                th_ogentone2=Thread(target=t3, args=(7, bot1.bgentone2,int(com[1]),str(com[2]),float(com[3])))
+                th_ogentone2.start()
+                ret=status                                           
             else:
                 ret =  "Incorrect Parameters"  
 
@@ -283,15 +417,19 @@ while True:
             elif param_cnt==1:
                 ret = bot1.brestnote()                
             elif param_cnt==2:
-                ret = bot1.brestnote(float(com[1]))
+                th_orestnote=Thread(target=t1, args=(8, bot1.brestnote,int(com[1])))
+                th_orestnote.start() 
+                ret=status                  
             else:
                 ret =  "Incorrect Parameters"  
 
         elif com[0]=='maru_odance':
             if 'bot1' not in locals():
                 ret = "Not Connected"  
-            elif param_cnt==1:                           
-                ret = bot1.bdance()                  
+            elif param_cnt==1:                            
+                th_odance=Thread(target=td, args=(9,))
+                th_odance.start() 
+                ret=status                                     
             else:
                 ret =  "Incorrect Parameters"     
 
@@ -309,20 +447,31 @@ while True:
             elif param_cnt==1:                           
                 ret = bot1.bflashrainbow()                  
             elif param_cnt==2:                           
-                 ret = bot1.bflashrainbow(int(com[1]))                  
+                th_oflashrainbow=Thread(target=t1, args=(10, bot1.bflashrainbow,int(com[1])))
+                th_oflashrainbow.start() 
+                ret=status                                     
             else:
                 ret =  "Incorrect Parameters"  
 
         elif com[0]=='maru_ozigzag':
             if 'bot1' not in locals():
                 ret = "Not Connected"                  
-            elif param_cnt==3:                           
-                ret = bot1.bzigzag(int(com[1]),int(com[2])) 
-            elif param_cnt==4:
-                ret = bot1.bzigzag(int(com[1]),int(com[2]),int(com[3]))                  
+            elif param_cnt==2:                           
+                th_ozigzag=Thread(target=tz, args=(11,90,int(com[1])))                
+                th_ozigzag.start()
+                ret=status                  
+            elif param_cnt==3: 
+                th_ozigzag=Thread(target=tz, args=(12,90,int(com[1]),int(com[2])))
+                th_ozigzag.start()
+                ret=status                  
             else:
                 ret =  "Incorrect Parameters"  
 
+        elif com[0]=='maru_ocheck':
+            if param_cnt==1:                           
+                ret = status
+            else:
+                ret =  "Incorrect Parameters" 
 
         elif com[0]=='maru_oversion':
             if param_cnt==1:                           
@@ -342,7 +491,7 @@ while True:
         else:
             ret="Unknown command"
 
-        log("Return:{}".format(ret))            
+        log("Return:{}\n\r".format(ret))            
         client_socket.send(str(ret).encode())
 
     if data =='quit' or data=='maru_oquit':       
